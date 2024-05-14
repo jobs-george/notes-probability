a85cddf3b15019ba3344aab80e0a39768f64dc6c
George Fitton <George.Fitton@Unilever.com>
2024-05-14 17:17:10 +0100

    added R code

--- 08-inequalities/code/inequalities.R
+++ 08-inequalities/code/inequalities.R
@@ -1,1 +1,27 @@
-# some R code
+# simulate
+
+set.seed(42)
+
+n <- 10^3
+n_vec <- 1:n
+
+simulations <- sample(c(0,1), size = n, replace = TRUE)
+
+moving_average <- cumsum(simulations) / n_vec
+
+plot(moving_average, ylim = c(0,1), type = "l", xlab = "n", ylab = "Sample Average", main = "Bern(1/2)")
+curve(0.5 + sqrt(1/(4*x)), from = 0, to = 1000, col = "red", add = TRUE)
+curve(0.5 - sqrt(1/(4*x)), from = 0, to = 1000, col = "red", add = TRUE)
+
+# simulate
+
+set.seed(13)
+
+n <- 10^3
+n_vec <- 1:n
+
+simulations <- rcauchy(n)
+
+moving_average <- cumsum(simulations) / n_vec
+
+plot(moving_average, type = "l", xlab = "n", ylab = "Sample Average", main = "Cauchy(0,1)")
